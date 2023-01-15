1c8aef561602138005813831804bdc958e99a1df
Tommy <tomsan@alu.centroafuera.es>
2023-01-15 13:19:44 +0100

    Commit ( 13:19 ------> 15/01/2023)
    Mejora en las páginas
    Enlaze con Firebase

--- app/src/main/java/com/example/buscador_amigos_android/MainActivity.kt
+++ app/src/main/java/com/example/buscador_amigos_android/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.buscador_amigos_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.login).setOnClickListener { acceder() }
+        findViewById<Button>(R.id.crearCuenta).setOnClickListener { cambioPagina() }
+
+
+
+
+    }
+
+    private fun checkNombre(): Boolean {
+
+        return true
+
+    }
+
+    private fun checkContraseña(): Boolean {
+
+        return true
+    }
+    private fun acceder() {
+
+    }
+    private fun cambioPagina() {
+
     }
 }
